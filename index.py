e335bdf9f586a4f69fdc91a30326c76563ea274c
Igor Mascarenhas <106632879+Igorodri@users.noreply.github.com>
2025-03-07 11:44:44 -0300

    Adição de relatório de Leadtime

--- index.py
+++ index.py
@@ -39,78 +39,192 @@ def gerar_csv():
         data_fim = registro.get('data-fim')
 
         if not data_inicio or not data_fim:
-            return jsonify({'erro': 'Data Início ou Data Fim não inseridos.'})
+            print("Data Início ou Data Fim não inseridos.")
+            return jsonify({'erro': 'Data Início ou Data Fim não inseridos.'}), 404
         
 
-        # query = f"""
-        #         SELECT
-        #             ACTION_CARD_ID cardId,
-        #             CARD_CREATION_DATE,
-        #             CARD_NAME,
-        #             CARD_ID_SHORT,
-        #             MAX(ACTION_DATE) AS CONCLUSION_DATE
-        #         FROM
-        #             ACTIONS
-        #         INNER JOIN
-        #             CARDS ON CARD_ID = ACTION_CARD_ID
-        #         WHERE
-        #             ACTION_LIST_AFTER IN (
-        #                 SELECT RULES_TRELLO_OBJECT_ID
-        #                 FROM RULES
-        #                 WHERE RULES_KEY = 'doneList'
-        #                 AND RULES_ACTIVE = 1
-        #             )
-        #             AND ACTION_DATE BETWEEN {data_inicio} AND {data_fim}
-        #         GROUP BY
-        #             ACTION_CARD_ID
+        query = """
+                SELECT
+                    ACTION_CARD_ID BS_CARD_ID,
+                    CARD_CREATION_DATE,
+                    CARD_NAME,
+                    CARD_ID_SHORT,
+                    MAX(ACTION_DATE) AS CONCLUSION_DATE
+                FROM
+                    ACTIONS
+                INNER JOIN
+                    CARDS ON CARD_ID = ACTION_CARD_ID
+                WHERE
+                    ACTION_LIST_AFTER IN (
+                        SELECT RULES_TRELLO_OBJECT_ID
+                        FROM RULES
+                        WHERE RULES_KEY = 'doneList'
+                        AND RULES_ACTIVE = 1
+                    )
+                    AND ACTION_DATE BETWEEN %s AND %s
+                GROUP BY
+                    ACTION_CARD_ID
 
-        #         UNION ALL
+                UNION ALL
 
-        #         SELECT
-        #             ACTION_CARD_ID cardId,
-        #             CARD_CREATION_DATE,
-        #             CARD_NAME,
-        #             CARD_ID_SHORT,
-        #             MAX(ACTION_DATE) AS CONCLUSION_DATE
-        #         FROM
-        #             ACTIONS
-        #         INNER JOIN
-        #             CARDS ON CARD_ID = ACTION_CARD_ID
-        #         WHERE
-        #             ACTION_LIST_AFTER IN (
-        #                 SELECT RULES_TRELLO_OBJECT_ID
-        #                 FROM RULES
-        #                 WHERE RULES_KEY = 'doneList'
-        #                 AND RULES_ACTIVE = 0
-        #             )
-        #             AND ACTION_DATE BETWEEN {data_inicio} AND {data_fim}
-        #         GROUP BY
-        #             ACTION_CARD_ID;
-        #     """
+                SELECT
+                    ACTION_CARD_ID BS_CARD_ID,
+                    CARD_CREATION_DATE,
+                    CARD_NAME,
+                    CARD_ID_SHORT,
+                    MAX(ACTION_DATE) AS CONCLUSION_DATE
+                FROM
+                    ACTIONS
+                INNER JOIN
+                    CARDS ON CARD_ID = ACTION_CARD_ID
+                WHERE
+                    ACTION_LIST_AFTER IN (
+                        SELECT RULES_TRELLO_OBJECT_ID
+                        FROM RULES
+                        WHERE RULES_KEY = 'doneList'
+                        AND RULES_ACTIVE = 0
+                    )
+                    AND ACTION_DATE BETWEEN %s AND %s
+                GROUP BY
+                    ACTION_CARD_ID;
+            """
 
-        query = "SELECT ACTION_CARD_ID cardId, MAX(ACTION_DATE) AS CONCLUSION_DATE FROM ACTIONS WHERE ACTION_LIST_AFTER = '670d1616ad6d3d830c285c41' AND ACTION_DATE BETWEEN %s AND %s GROUP BY  ACTION_CARD_ID"
+        # query = "SELECT ACTION_CARD_ID BS_CARD_ID, MAX(ACTION_DATE) AS CONCLUSION_DATE FROM ACTIONS WHERE ACTION_LIST_AFTER = '670d1616ad6d3d830c285c41' AND ACTION_DATE BETWEEN %s AND %s GROUP BY  ACTION_CARD_ID"
 
         print("Data Inicial: " + data_inicio)
         print("Data Final: " + data_fim)
 
         with db.engine.connect() as conexao:
+            df = pd.read_sql(query, conexao, params=(data_inicio, data_fim, data_inicio, data_fim))
+
+            print(df)
+
+
+            data_cards = requests.get(os.getenv('TRELLO_CARDS_URL'))
+
+            data_lists = requests.get(os.getenv('TRELLO_LISTS_URL'))
+
+            board_cards = data_cards.json()
+            data_lists = data_lists.json()
+
+            lists = [{'idList': lst['id'], 'listName': lst['name']} for lst in data_lists]
+            lists_df = pd.DataFrame(lists)
+
+            lists_filter = ['670d1616ad6d3d830c285c41', '62388db5b91b032488cea097']
+
+            cards = []
+            for card in board_cards:
+                if card['labels']:
+                    for label in card['labels']:
+                        cards.append({
+                            'BS_CARD_ID': card['id'],
+                            'cardName': card['name'],
+                            'cardDesc': card['desc'],
+                            'idList': card['idList'],
+                            'idShort': card['idShort'],
+                            'shortUrl': card['shortUrl'],
+                            'idLabel': label['id'],
+                            'labelName': label['name'],
+                            'labelColor': label['color']
+                        })
+                else:
+                    cards.append({
+                        'BS_CARD_ID': card['id'],
+                        'cardName': card['name'],
+                        'idList': card['idList'],
+                        'idShort': card['idShort'],
+                        'shortUrl': card['shortUrl'],
+                        'idLabel': '',
+                        'labelName': '',
+                        'labelColor': ''
+                    })
+
+            cards_df = pd.DataFrame(cards)
+            cards_df = cards_df[cards_df['idList'].isin(lists_filter)]
+
+            merge_df = pd.merge(cards_df, lists_df, on='idList')
+            merge_df = pd.merge(merge_df, df, on='BS_CARD_ID')
+
+            done_list_df = merge_df[merge_df['labelColor'] == 'lime']
+
+            
+            wb = Workbook()
+            ws = wb.active
+
+            
+            for r_idx, row in enumerate(dataframe_to_rows(done_list_df, index=False, header=True), 1):
+                for c_idx, value in enumerate(row, 1):
+                    ws.cell(row=r_idx, column=c_idx, value=value)
+
+            
+            alignment = Alignment(horizontal='center', vertical='center')
+            border = Border(left=Side(border_style='thin'),
+                            right=Side(border_style='thin'),
+                            top=Side(border_style='thin'),
+                            bottom=Side(border_style='thin'))
+
+            for row in ws.iter_rows(min_row=1, max_row=ws.max_row):
+                ws.row_dimensions[row[0].row].height = 30
+                for cell in row:
+                    ws.column_dimensions[cell.column_letter].width = 30
+                    cell.alignment = alignment
+                    cell.border = border
+
+            
+            output = io.BytesIO()
+            wb.save(output)
+            output.seek(0)
+
+            return send_file(output, as_attachment=True, download_name=f"entregas_da_semana_{data_inicio}_a_{data_fim}.xlsx", mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+
+    except Exception as e:
+        print("Erro ao executar requisição: ", str(e))
+        print(traceback.format_exc());
+        return jsonify({'erro': str(e)}), 500
+
+
+@app.route('/leadtime', methods=['POST'])
+def gerar_leadtime():
+    try:
+        registro = request.get_json()
+        data_inicio = registro.get('data-inicio')
+        data_fim = registro.get('data-fim')
+
+        if not data_inicio or not data_fim:
+            print("Data Início ou Data Fim não inseridos.")
+            return jsonify({'erro': 'Data Início ou Data Fim não inseridos.'}), 404
+
+        query = """
+            SELECT ACTION_CARD_ID AS cardId,
+                   CARD_CREATION_DATE,
+                   MAX(ACTION_DATE) AS CONCLUSION_DATE
+            FROM ACTIONS
+            INNER JOIN CARDS ON CARD_ID = ACTION_CARD_ID
+            WHERE ACTION_LIST_AFTER = '62388db5b91b032488cea097'
+            AND ACTION_DATE BETWEEN %s AND %s 
+            GROUP BY ACTION_CARD_ID, CARD_CREATION_DATE
+        """
+
+        print(f"Data Início: {data_inicio}")
+        print(f"Data Fim: {data_fim}")
+
+        with db.engine.connect() as conexao:
             df = pd.read_sql(query, conexao, params=(data_inicio, data_fim))
+            print(df)
 
         data_cards = requests.get(os.getenv('TRELLO_CARDS_URL'))
-
         data_lists = requests.get(os.getenv('TRELLO_LISTS_URL'))
 
         board_cards = data_cards.json()
         data_lists = data_lists.json()
 
-        lists = [{'idList': lst['id'], 'listName': lst['name']} for lst in data_lists]
+        lists = [{'idList': lista['id'], 'listName': lista['name']} for lista in data_lists]
         lists_df = pd.DataFrame(lists)
-
-        lists_filter = ['670d1616ad6d3d830c285c41']
+        lists_filter = ['62388db5b91b032488cea097']
 
         cards = []
         for card in board_cards:
-            if card['labels']:
+            if card.get('labels'):
                 for label in card['labels']:
                     cards.append({
                         'cardId': card['id'],
@@ -141,18 +255,21 @@ def gerar_csv():
         merge_df = pd.merge(cards_df, lists_df, on='idList')
         merge_df = pd.merge(merge_df, df, on='cardId')
 
-        done_list_df = merge_df[merge_df['labelColor'] == 'lime']
+        clients_label_filter = merge_df['idLabel'].isin([
+            '63ce80a5ed837d03dc64c23b', 
+            '63ce808818d9320640a435d2', 
+            '63ce80943c3d45017d481a5b'
+        ])
 
-        
+        done_list_df = merge_df[clients_label_filter].copy()
+        done_list_df.drop(['cardId', 'idList', 'idLabel', 'labelColor', 'listName'], axis=1, inplace=True)
+
         wb = Workbook()
         ws = wb.active
 
-        
-        for r_idx, row in enumerate(dataframe_to_rows(done_list_df, index=False, header=True), 1):
-            for c_idx, value in enumerate(row, 1):
-                ws.cell(row=r_idx, column=c_idx, value=value)
+        for r in dataframe_to_rows(done_list_df, index=False, header=True):
+            ws.append(r)
 
-        
         alignment = Alignment(horizontal='center', vertical='center')
         border = Border(left=Side(border_style='thin'),
                         right=Side(border_style='thin'),
@@ -162,22 +279,20 @@ def gerar_csv():
         for row in ws.iter_rows(min_row=1, max_row=ws.max_row):
             ws.row_dimensions[row[0].row].height = 30
             for cell in row:
-                ws.column_dimensions[cell.column_letter].width = 30
-                cell.alignment = alignment
-                cell.border = border
+                    ws.column_dimensions[cell.column_letter].width = 30
+                    cell.alignment = alignment
+                    cell.border = border
 
-        
         output = io.BytesIO()
         wb.save(output)
         output.seek(0)
 
-        return send_file(output, as_attachment=True, download_name=f"entregas_da_semana_{data_inicio}_a_{data_fim}.xlsx", mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
-
+        return send_file(output, as_attachment=True, download_name=f"leadtime_por_complexidade_{data_inicio}_a_{data_fim}.xlsx", mimetype="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet")
+    
     except Exception as e:
-        print("Erro ao executar requisição:", str(e))
-        print(traceback.format_exc());
+        print(f"Erro ao executar requisição: {str(e)}")
+        print(traceback.format_exc())
         return jsonify({'erro': str(e)}), 500
-
 
 @app.route('/favicon.ico')
 def favicon():
--- static/script.js
+++ static/script.js
@@ -1,4 +1,7 @@
-const inputsArray = [document.getElementById("data-inicio"), document.getElementById("data-fim")];
+const data_inicio_element = document.getElementById("data-inicio");
+const data_fim_element = document.getElementById("data-fim");
+const data_inicio_lead_element = document.getElementById("data-inicio_lead");
+const data_fim_lead_element = document.getElementById("data-fim_lead");
 
 
 function selecao_relatorios(){
@@ -34,10 +37,9 @@ function selecao_relatorios(){
 selecao_relatorios()
 
 
-
-
-
-function validacao_inputs() {
+function validacao_inputs(inicio_element, fim_element) {
+  const inputsArray = [];
+  inputsArray.append(inicio_element,fim_element)
     for (let i = 0; i < inputsArray.length; i++) {
         const input = inputsArray[i];
         if (input.value.trim() === '') {
@@ -194,9 +196,139 @@ document.getElementById("form_cards").addEventListener("submit", function(event)
       return;
   }
 
-  validacao_inputs(); 
+  validacao_inputs(data_inicio_element, data_fim_element); 
   gerar_csv(data_inicio, data_fim);
 });
+
+function gerar_leadtime(data_inicio, data_fim){
+  fetch('/leadtime', {
+    method: 'POST',
+    headers: {
+          'Content-Type': 'application/json'
+    },
+
+    body: JSON.stringify({
+      "data-inicio": data_inicio,
+      "data-fim": data_fim
+    })     
+  })
+  .then(response => {
+    if(!response.ok){
+      throw new Error("Erro ao gerar relatório de cards");
+    }else{
+      return response.blob();
+    }
+  })
+  .then(blob => {
+    Toastify({
+        text: "Gerando Relatório...",
+        duration: 5000,
+        destination: "",
+        newWindow: true,
+        close: true,
+        gravity: "top",
+        position: "right", 
+        stopOnFocus: true, 
+        style: {
+          background: "linear-gradient(to right, #00b09b, #96c93d)"
+        },
+        onClick: function(){} 
+      }).showToast();
+    console.log("Gerando relatório...");
+    for(let i =0; i < inputsArray.length; i++){
+      const input = inputsArray[i];
+      setTimeout(function(){
+        input.style.borderColor = '#bdc3c7'; 
+      },1500)
+    }
+
+    
+      setTimeout(function(){
+        const url = window.URL.createObjectURL(blob);
+        const a = document.createElement("a");
+        a.href = url;
+        a.download = "Relatório de Leadtime " + data_inicio + " -  "  + data_fim +  ".xlsx";
+        document.body.appendChild(a);
+        a.click();
+        a.remove();
+        
+        Toastify({
+            text: "Relatório Baixado com Sucesso!",
+            duration: 5000,
+            destination: "",
+            newWindow: true,
+            close: true,
+            gravity: "top",
+            position: "right", 
+            stopOnFocus: true, 
+            style: {
+              background: "linear-gradient(to right, #00b09b, #96c93d)"
+            },
+            onClick: function(){} 
+          }).showToast();
+        console.log("Relatório baixado com sucesso!");
+      },1500)
+    
+  })
+  .catch(error => {
+      Toastify({
+          text: "Erro ao gerar Relatório.",
+          duration: 5000,
+          destination: "",
+          newWindow: true,
+          close: true,
+          gravity: "top", 
+          position: "right", 
+          stopOnFocus: true, 
+          style: {
+            background: "linear-gradient(to right, #ff0000, #ec5353)"
+          },
+          onClick: function(){} 
+        }).showToast();
+      console.error("Erro ao gerar relatório:", error);
+  });
+}
+
+document.getElementById("form_lead").addEventListener("submit", function(event) {
+  event.preventDefault();
+
+  const data_inicio = document.getElementById("data-inicio_lead").value;
+  const data_fim = document.getElementById("data-fim_lead").value;
+
+  if (isNaN(data_inicio) || isNaN(data_fim)) {
+    validacao_inputs();   
+  }
+
+  if (data_inicio > data_fim) {
+      Toastify({
+          text: "Data de Início não pode ser maior do que a Data Final.",
+          duration: 5000,
+          destination: "",
+          newWindow: true,
+          close: true,
+          gravity: "top", 
+          position: "right",
+          stopOnFocus: true, 
+          style: {
+            background: "linear-gradient(to right, #ff0000, #ec5353)"
+          },
+          onClick: function(){} 
+        }).showToast();
+      document.getElementById("data-inicio_lead").style.borderColor = 'red';
+      document.getElementById("data-fim_lead").style.borderColor = 'red';
+
+      setTimeout(function(){
+        document.getElementById("data-inicio_lead").style.borderColor = '#bdc3c7';
+        document.getElementById("data-fim_lead").style.borderColor = '#bdc3c7'; 
+      }, 3000);
+      return;
+  }
+
+  validacao_inputs(data_inicio_lead_element, data_fim_lead_element);
+  gerar_leadtime(data_inicio, data_fim)
+})
+
+
 
 
 
